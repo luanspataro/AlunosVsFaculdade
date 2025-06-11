887322dd3d13ef322ffebe757485e5c5f09e9b7d
Bruno Lee <lee.bruno23@gmail.com>
2025-06-11 01:25:25 -0300

    feat: quantidade de alunos dinamico

--- src/main/java/com/simulacao/AlunosVsFaculdade/service/SimuladorService.java
+++ src/main/java/com/simulacao/AlunosVsFaculdade/service/SimuladorService.java
@@ -11,7 +11,8 @@ public class SimuladorService {
     private Thread simulacaoThread;
     private Simulador simulador;
 
-    public synchronized void iniciarSimulacao(int inteligencia, int dificuldade, int velocidade) {
+    public synchronized void iniciarSimulacao(
+        int inteligencia, int dificuldade, int velocidade, int quantidadeAlunos) {
         if (simulacaoThread != null && simulacaoThread.isAlive()) {
             simulador.interromper();
             try {
@@ -20,22 +21,17 @@ public class SimuladorService {
                 Thread.currentThread().interrupt();
             }
         }
+
         Tabuleiro.agentes.clear();
         Tabuleiro.iniciaTabuleiro();
 
-        Aluno aluno1 = new Aluno("Maria", 2, 3, inteligencia);
-        Aluno aluno2 = new Aluno("João", 5, 6, inteligencia);
-
         simulador = new Simulador();
-        simulador.adicionarAluno(aluno1);
-        simulador.adicionarAluno(aluno2);
-
-        Tabuleiro.adicionaAgente(aluno1);
-        Tabuleiro.adicionaAgente(aluno2);
+        simulador.adicionarAlunosAleatorios(quantidadeAlunos, inteligencia);
 
         simulacaoThread = new Thread(() -> simulador.comecarSimulacao(dificuldade, velocidade));
         simulacaoThread.start();
     }
+
 
     public String[][] getMapaAtual() {
         return Tabuleiro.mapa;
